f2cf2686a513cacce3b1ecb4aab6a25c1954e2a9
VladHurtin <novogodble@gmail.com>
2020-05-14 20:19:17 +0400

    Chage GET to POST in controllers

--- SovcomPushService/src/main/java/com/example/SovcomPushService/controller/PushNotificationController.java
+++ SovcomPushService/src/main/java/com/example/SovcomPushService/controller/PushNotificationController.java
@@ -2,13 +2,14 @@ package com.example.SovcomPushService.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/push")
 public class PushNotificationController {
-    @GetMapping("/notification")
+    @PostMapping("/notification")
     public String pushNotification() {
         return "Мы отправили вам push уведомление! Наверное...";
     }
